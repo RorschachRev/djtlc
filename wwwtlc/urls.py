6966dd401aa13877954bc40a0131ed686d9e0b05
RorschachRev <rorschachrev@gmail.com>
2018-04-30 14:13:39 -0600

    Working base

--- wwwtlc/urls.py
+++ wwwtlc/urls.py
@@ -19,8 +19,8 @@ from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name = 'home.html'), name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
+    url(r'^$', TemplateView.as_view(template_name = 'pages/home.html'), name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'pages/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'template_name': 'pages/logout.html'}, name='logout'),
     url(r'^admin/', admin.site.urls),
 ]
