8e2b36c7879c7ff4b3cec44731f58508302eefae
Alex <alexheistdev@gmail.com>
2018-05-15 14:44:48 -0600

    djmoney implementing - changes in changelog.txt

--- wwwtlc/settings.py
+++ wwwtlc/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import moneyed
+from moneyed.localization import _FORMATTER
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -41,6 +43,7 @@ INSTALLED_APPS = [
     'login',
     'loan',
     'formtools',
+    'djmoney',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en_US'
 
 TIME_ZONE = 'UTC'
 
