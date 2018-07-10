8c1f14572fc822c47eba87baf7927c37878b7938
Alex <alexheistdev@gmail.com>
2018-07-10 11:33:09 -0600

    settings changes

--- wwwtlc/settings.py
+++ wwwtlc/settings.py
@@ -25,11 +25,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '67(nyof2+$($n1*g8d9p8#c)53fssm3(7)b)fcb@@ogdeutx0-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -76,17 +71,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'wwwtlc.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
