d990b7a90f81df3d37f14ef8fc86d8c0c4964610
Alex <alexheistdev@gmail.com>
2018-09-21 11:50:28 -0600

    minor changes

--- wwwtlc/views.py
+++ wwwtlc/views.py
@@ -729,11 +729,11 @@ class LoanApplyWizard(SessionWizardView):
 				'Greeting ' + a.name_first + ' ' + a.name_last + ',\n\nThe LendingCoin, Inc. received your Commercial Loan Refinancing Expression of Interest and is processing the information you submitted. It has been routed to the refinancing committee for review and you will be hearing from us soon.\n\nAt The LendingCoin, Inc., we are excited to be able to provide you and others with the opportunity to be considered for this alternative to traditional refinancing that allows you better terms, quicker and more responsive considerations, and the benefit of the transparency of the blockchain empowered processes employed at The LendingCoin, Inc.\n\nBase on your requested loan, a loan of $' + str(d.amount_desired) + ' for ' + str(d.TERM_CHOICES[d.term_desired][1])+ '(s) at ' + str(d.intrate_desired) + '% would make your payment approximately $' + 'TODO' + '.\n\nWe look forward to disussing your refinancing needs in detail. If you have any need to talk to us before we are able to contact you, please don\'t hesitate.\n\nSincerely,\n\nDavid Slonaker\nChief Financial Officer', 
 	
 				# 'from' email address
+				# doesn't seem to work, but defaults to settings
 				'no_reply@thelendingcoin.com',
 				
 				# recipient email address
 				['loan-app@thelendingcoin.com']
-				#['finance@thelendingcoin.com', 'lender@thelendingcoin.com', 'cto@mediacoin.stream']
 			)
 			
 		return render(self.request, 'pages/loan_apply_done.html', {'name': a.name_first + ' ' + a.name_last} )
