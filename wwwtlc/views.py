ee6238134a777749d989bf6f235d3f9157c89fa9
Alex <alexheistdev@gmail.com>
2018-09-28 11:08:58 -0600

    added quoc and paul to email list - changes in local_settings as well

--- wwwtlc/views.py
+++ wwwtlc/views.py
@@ -743,10 +743,10 @@ class LoanApplyWizard(SessionWizardView):
 				'Greeting ' + a.name_first + ' ' + a.name_last + ',\n\nThe LendingCoin, Inc. received your Commercial Loan Refinancing Expression of Interest and is processing the information you submitted. It has been routed to the refinancing committee for review and you will be hearing from us soon.\n\nAt The LendingCoin, Inc., we are excited to be able to provide you and others with the opportunity to be considered for this alternative to traditional refinancing that allows you better terms, quicker and more responsive considerations, and the benefit of the transparency of the blockchain empowered processes employed at The LendingCoin, Inc.\n\nBase on your requested loan, a loan of $' + str_amount + ' for ' + str_term + '(s) at ' + str_intrate + '% would make your payment approximately $' + str(payment_calc(d.amount_desired, d.intrate_desired, int(split_term))) + '.\n\nWe look forward to disussing your refinancing needs in detail. If you have any need to talk to us before we are able to contact you, please don\'t hesitate.\n\nSincerely,\n\nDavid Slonaker\nChief Financial Officer\n\nThe LendingCoin, Inc.\n1550 S. Cloverdale Rd.\nBoise, ID 83709\n(208) 401-9596\nthelendingcoin.com', 
 	
 				# 'from' email address
-				'loan-app@thelendingcoin.com',
+				'loan-dept@thelendingcoin.com',
 				
 				# 'to' email address(es)
-				[a.email_address, 'loan-app@thelendingcoin.com', 'rich@thelendingcoin.com']
+				[a.email_address, 'loan-dept@thelendingcoin.com', 'rich@thelendingcoin.com', 'quoc@thelendingcoin.com', 'paul@thelendingcoin.com']
 			)
 			
 			# sends email (to STAFF) when data is submitted and validated
@@ -787,10 +787,10 @@ class LoanApplyWizard(SessionWizardView):
 				'\n\nAt your earliest opportunity, please review their Expression of Interest and process their submission as appropriate. This Expression of Interest can be reviewed by logging into The LendingCoin, Inc. Refinancing Dashboard and navigating to \'Loan Requests\'\n\nRegards,\n\nThe LendingCoin, Inc.',
 
 				# 'from' email address
-				'loan-app@thelendingcoin.com',
+				'loan-dept@thelendingcoin.com',
 
 				# 'to' email address(es)
-				['loan-app@thelendingcoin.com', 'rich@thelendingcoin.com']
+				['loan-dept@thelendingcoin.com', 'rich@thelendingcoin.com', 'quoc@thelendingcoin.com', 'paul@thelendingcoin.com']
 			)	
 
 		return render(self.request, 'pages/loan_apply_done.html', {'name': a.name_first + ' ' + a.name_last} )
